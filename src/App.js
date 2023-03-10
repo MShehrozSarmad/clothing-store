ed56ee7b08b10417665e230dc5c70e927f19dccc
MShehrozSarmad <shehrozm107@gmail.com>
2023-03-10 08:28:54 +0500

    react roouter dom and components divisions

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import React from "react";
-import "./categories.styles.scss";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/routes/home/home.component";
+
+const Shop = () => {
+  return <h1>im a shop page</h1>
+}
 
 const App = () => {
-
-  const categories = [
-    {
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      id: 1,
-    },
-    {
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      id: 2,
-    },
-    {
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      id: 3,
-    },
-    {
-      title: "Women",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      id: 4,
-    },
-    {
-      title: "Men",
-      imageUrl: "https://i.tbb.co/GCCdy8t/mens.png",
-      id: 5,
-    },
-  ];
-
-  return (
-    <div className="categories-container">
-      {categories.map(({ title, imageUrl, id }) => (
-          <div key={id} className="category-container">
-            {/* <img src="" alt="" /> */}
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        ))}
-    </div>
+  return(
+    <Routes>
+      <Route path="/home" element={<Home/>}>
+        <Route path="shop" element={<Shop/>}/>
+      </Route>
+    </Routes>
   );
 }
 
