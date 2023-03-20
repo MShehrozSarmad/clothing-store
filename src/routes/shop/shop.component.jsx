e35106473282df59995ad451f9056d72a0afa1f5
MShehrozSarmad <shehrozm107@gmail.com>
2023-03-20 22:43:31 +0500

    updated

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,15 @@
-import { ProductsContext } from "../../contexts/product.context";
 import { useContext } from "react";
-
+import { ProductsContext } from "../../contexts/product.context";
+import ProductCard from "../../components/product-card/product-card.component";
+import './shop.styles.scss';
 
 const Shoop = () => {
-    const {products} = useContext(ProductsContext);
-    return(
-        <div>
-            {products.map(({id,name}) => <h1 key={id}>{name}</h1>)}
+    const { products } = useContext(ProductsContext);
+    return (
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
         </div>
     );
 };
