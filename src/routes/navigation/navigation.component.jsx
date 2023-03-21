adc8564d5c673a6a0a09926ff3265f8ac3f4c815
MShehrozSarmad <shehrozm107@gmail.com>
2023-03-21 21:18:49 +0500

    cart icon functionality

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,10 +5,12 @@ import {UserContext} from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 import './navigation.style.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const {isCartOpen} = useContext(CartContext);
     return(
         <Fragment>
             <div className="navigation">
@@ -26,7 +28,7 @@ const Navigation = () => {
                     )}
                     <CartIcon/>
                 </div>
-                <CartDropDown/>
+                {isCartOpen && <CartDropDown/>}
             </div>
             <Outlet/>
         </Fragment>
